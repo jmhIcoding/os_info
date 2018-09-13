cb7a5dd0a2fb69f97b564312e8677b48040cd07a
jmh081701 <1024836002@qq.com>
2018-09-13 12:24:57 +0800

    ok

--- client.py
+++ client.py
@@ -2,20 +2,30 @@ __author__ = 'jmh081701'
 import  requests
 import  time
 import  os
+from multiprocessing import  Process
 import  copy
+import json
 req_json={
     "timestamp":0,
     "info":""
 }
+def fox():
+    fox=os.popen("firefox http://210.77.16.21")
 while True:
-    os.popen("firefox http://210.77.16.21")
+    #fox=os.popen("firefox http://210.77.16.21")
+    p=Process(target=fox)
+    p.start()
     posturl="http://67.218.148.94:801/post/osinfo"
     time.sleep(5)
-    os.close()
+    p.terminate()
+    p.join()
     req=copy.deepcopy(req_json)
     req['timestamp']=time.time()
     ip = os.popen("ifconfig eth0")
-    msg =ip.readall()
+    msg =ip.readlines()
     req['info']=str(msg)
-    requests.post(url=posturl,json=req)
-    sleep(30)
+    headers = {"Content-Type": "application/json"}
+    requests.post(url=posturl,data=json.dumps(req),headers=headers)
+    print("post well")
+    print(req)
+    time.sleep(30)
